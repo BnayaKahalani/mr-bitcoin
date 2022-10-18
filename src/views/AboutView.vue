2481babf14b631d6152bbe8742826113d5181a4e
Bnaya Kahalani <bnaya50@gmail.com>
2022-10-18 16:25:40 +0300

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="about">
+    <h1>Hello from about</h1>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="home">
+    <h1>Hello from home</h1>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
